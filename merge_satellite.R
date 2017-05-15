8e48ee8d20fa354056b3a76ae8f205f9fa8eef49
MLezamaValdes <mlezamavaldes@gmail.com>
2017-05-15 18:56:42 +0200

    read_ready

--- merge_satellite.R
+++ merge_satellite.R
@@ -1,28 +1,15 @@
-#merge satellite data Molopo
+#MERGE SATELLITE DATA MOLOPO
+
 library(raster)
 
+#paths
 main <- "E:/HK_Geoinfo/"
 data <- paste0(main, "data/")
-
-#test
-drgb <- paste0(data, "2013_rgb/")
-files <- list.files(drgb)
-
-s1 <- stack(paste0(drgb,files[2]))
-s2 <- stack(paste0(drgb,files[4]))
-s3 <- stack(paste0(drgb,files[6]))
-
-s1@
-bstack <- merge(s1,s2,s3)
-
-
-rgb2013 <- merge(s1,s2,s3)
-writeRaster(rgb2013, paste0(drgb,"rgb2013.tif"))
 
 #L8 2013
 #no, nw, so indicate tile position; d=dry season, r = rainy season
 
-#dry season
+#DRY SEASON
 nod <- paste0(data,"173078/LC08_L1TP_173078_20130627_20170504_01_T1/")
 nwd <- paste0(data,"174078/LC08_L1TP_174078_20130720_20170503_01_T1/")
 sod <- paste0(data,"173079/LC08_L1TP_173079_20130627_20170504_01_T1/")
@@ -30,19 +17,25 @@ sod <- paste0(data,"173079/LC08_L1TP_173079_20130627_20170504_01_T1/")
 bands <- c(2, 3, 4, 5, 6, 7, 10, 11)
 tiles <- c(nod, nwd, sod)
 
-i=2
-j=2
-
-#tb = tilebands; tdir = tiledirectory
+#build list of 3 stacks with all bands in all tiles
 tb <- character()
-stacklist <- lapply(seq(tiles), function (j){ #3x, für alle Kacheln
-  for (i in seq(bands)){ ##für jeden in Sequenz 2,3,4,...,10,11
+dst <- lapply(seq(tiles), function (j){
+  for (i in seq(bands)){ 
     tb[i] <- paste0(tiles[j],list.files(tiles[j], pattern =
                                           paste0("\\B",toString(bands[i]),".TIF$")))}
-  ###liste alle, die für die jeweilige Kachel die enstprechenden 
-  ###Bänder haben
-  st <- stack(tb) ##stacke alle Bänder einer Kachel
+  st <- stack(tb) 
 })
 
-plot(stacklist[[2]])
+#RAINY SEASON
+nor <- paste0(data,"173078/LC08_L1TP_173078_20131204_20170428_01_T1/")
+nwr <- paste0(data,"174078/LC08_L1TP_174078_20131227_20170427_01_T1/")
+sor <- paste0(data,"173079/LC08_L1TP_173079_20131204_20170428_01_T1/")
 
+#build list of 3 stacks with all bands in all tiles
+tb <- character()
+rst <- lapply(seq(tiles), function (j){
+  for (i in seq(bands)){ 
+    tb[i] <- paste0(tiles[j],list.files(tiles[j], pattern =
+                                          paste0("\\B",toString(bands[i]),".TIF$")))}
+  st <- stack(tb)
+})
